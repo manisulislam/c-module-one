2142d1404f0c35565fa663808db3ada137ec7340
anisul islam <anisulislam980340@gmail.com>
2023-05-21 08:38:46 +0600

    swithc case completed

---
+++ switch.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int value;
+    cin>>value;
+    switch(value){
+        
+        case 1:
+            cout<<"one"<<endl;
+            break;
+        case 2:
+            cout<<"two"<<endl;
+            break;
+        default:
+            cout<<"do no match";
+            break;
+
+    }
+
+}
